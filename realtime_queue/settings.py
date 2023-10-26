914209c529fbc02d29ff7721c55ea9b51983fafb
Alireza Feyzabadi <arfa79lg@gmail.com>
2023-10-26 13:09:21 +0330

    postgresql settings

--- realtime_queue/settings.py
+++ realtime_queue/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import psycopg2
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'task_queue',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,14 @@ WSGI_APPLICATION = 'realtime_queue.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'realtime',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': 'localhost',  
+        'PORT': '5432'
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
--- task_queue/models.py
+++ task_queue/models.py
@@ -1,3 +1,21 @@
-from django.db import models
+# taskqueue/models.py
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+class Task(models.Model):
+    STATUS_CHOICES = (
+        ('queued', 'Queued'),
+        ('processing', 'Processing'),
+        ('completed', 'Completed'),
+        ('failed', 'Failed'),
+    )
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    task_name = models.CharField(max_length=255)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='queued')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.task_name
